5af08366330a94f9304ed1f09ba796b782706147
anmol-gupta <anmolify@gmail.com>
2018-08-27 20:02:34 +0530

    Updated scripts to use filter buttons

--- scripts.js
+++ scripts.js
@@ -26,14 +26,7 @@ function paintToCanvas() {
 
 	return setInterval(() => {
 		ctx.drawImage(video, 0, 0, width, height);
-		let pixels = ctx.getImageData(0, 0, width, height);
-		ctx.globalAlpha = 0.1;
-		if(butt.name == 'RE'){
-		pixels = redEffect(pixels);
-	} else{
-		pixels = rgbSplit(pixels);
-	}
-		ctx.putImageData(pixels,0,0);
+		//let pixels = ctx.getImageData(0, 0, width, height);
 	}, 16);
 }
 
@@ -54,10 +47,43 @@ function takePhoto() {
 
 getVideo();
 
+function paintToCanvasRE() {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	console.log(width, height);
+	canvas.width = width;
+	canvas.height = height;
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+		let pixels = ctx.getImageData(0, 0, width, height);
+		ctx.globalAlpha = 0.1;
+		pixels = redEffect(pixels);
+		ctx.putImageData(pixels,0,0);
+	}, 16);
+}
+
+function paintToCanvasRGBS() {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	console.log(width, height);
+	canvas.width = width;
+	canvas.height = height;
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+		let pixels = ctx.getImageData(0, 0, width, height);
+		ctx.globalAlpha = 0.1;
+		pixels = rgbSplit(pixels);
+		ctx.putImageData(pixels,0,0);
+	}, 16);
+}
+
+
 function redEffect(pixels) {
 	for(var i = 0; i < pixels.data.length; i += 4){
-		pixels.data[i + 0] += 100;
-		pixels.data[i + 1] -+ 50;
+		pixels.data[i + 0] += 50;
+		//pixels.data[i + 1] -= 50;
 		pixels.data[i + 2] *= 0.5;
 	}
 	return pixels;
